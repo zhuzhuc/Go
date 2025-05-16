0d04d6f7c52fd5b7a479becdc554572b59f760e5
zhuzhuc <1727643988@qq.com>
2025-05-16 22:15:00 +0800

    Study of Goland

--- Read and Write/main.go
+++ Read and Write/main.go
@@ -113,4 +113,64 @@ func main() {
 		return
 	}
 	fmt.Println("写入完毕")
+	fmt.Println("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+	// 追加文件
+	// 1. 首先打开文件，获取文件句柄
+	// 2. 然后调用 file.Write() 方法，将内容写入文件
+	// 3. 如果写入成功，返回写入的字节数和 nil 错误
+	// 4. 如果写入失败，返回写入的字节数和错误信息
+	// 5. 最后关闭文件句柄
+	fmt.Println("追加文件")
+	www, err = os.OpenFile("/Users/Apple/Desktop/Go/Read and Write/1.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	defer www.Close() // 延迟关闭文件句柄,必须关闭文件
+	if err != nil {
+		fmt.Println("打开文件失败:", err)
+		return
+	}
+	_, err = www.Write([]byte(`
+                          '--'   '--'                           '--'   '--'                           '--'   '--'
+`))
+	if err != nil {
+		fmt.Println("写入文件失败:", err)
+		return
+	}
+	fmt.Println("写入完毕")
+	fmt.Println("-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+
+	// 追加文件内容，使用带缓冲的写入器
+	fsf, err := os.OpenFile("/Users/Apple/Desktop/Go/Read and Write/1.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	// 延迟关闭文件句柄，确保文件在函数结束时关闭
+	defer fsf.Close()
+	if err != nil {
+		fmt.Println("打开文件失败:", err)
+		return
+	}
+
+	// 创建带缓冲的写入器，使用新的变量名 writer
+	writer := bufio.NewWriter(fsf)
+
+	// 向缓冲区写入字符串
+	n, err := writer.WriteString(`
+	                          '--'   '--'                           '--'   '--'                           '--'   '--'`)
+	if err != nil {
+		fmt.Println("写入缓冲区失败:", err)
+		return
+	}
+	fmt.Printf("成功写入 %d 个字节到缓冲区\n", n)
+
+	// 刷新缓冲区，将内容写入文件
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println("刷新缓冲区失败:", err)
+		return
+	}
+	fmt.Println("缓冲区内容已成功写入文件")
+
+	str := "hello world"
+	errs := ioutil.WriteFile("/Users/Apple/Desktop/Go/Read and Write/2.txt", []byte(str), 0666)
+	if errs != nil {
+		fmt.Println("写入文件失败:", err)
+		return
+	}
+	fmt.Println("ioutil()写入完毕")
 }
